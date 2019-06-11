9c89bc58e546c0ce3dac1c868b87acfedf218801
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-06-11 08:13:22 +0100

    Add bulma

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,8 +29,17 @@ module.exports = (env, options) => ({
         }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.scss$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              { loader: 'css-loader' },
+              {
+                  loader: 'sass-loader',
+                  options: {
+                      sourceMap: true,
+                  }
+              }
+          ]
       }
     ]
   },
